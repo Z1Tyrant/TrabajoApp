2d8501b6a0811bcad861aeb03f1c046a1fe1b79d
Bryan <brya.contreras@duocuc.cl>
2023-10-23 15:15:56 -0300

    change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirige a la página de login por defecto
+  { path: '', redirectTo: 'product-add', pathMatch: 'full' }, // Redirige a la página de login por defecto
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule),
