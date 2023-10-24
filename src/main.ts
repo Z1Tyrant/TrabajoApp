77830ed5a884747c01e267d2a60b449a9f74dcca
Bryan <brya.contreras@duocuc.cl>
2023-10-24 02:27:48 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,18 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+import { error } from 'console';
 
 if (environment.production) {
   enableProdMode();
 }
 
+
+
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+// Call the element loader before the render call
+defineCustomElements(window);  
